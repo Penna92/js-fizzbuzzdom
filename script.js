7efe4f874c6ec3c976aed6aeb5332c947168e370
Penna92 <giacomopenna92@gmail.com>
2022-03-28 16:03:53 +0200

    style.css : added optional layout

--- script.js
+++ script.js
@@ -1,35 +1,32 @@
-const ul = document.createElement('ul');
-document.getElementById('app').append(ul);
+const ul = document.createElement("ul");
+document.getElementById("app").append(ul);
 
 // console.log(app);
 
 for (let i = 1; i <= 100; i++) {
   if (i % 3 === 0 && i % 5 !== 0) {
-    const li = document.createElement('li');
+    const li = document.createElement("li");
     li.append("Fizz");
     ul.append(li);
-    li.style.color = "red";
-    console.log("Fizz");   
+    li.style.background = "#0BD6A1";
+    console.log("Fizz");
   } else if (i % 3 !== 0 && i % 5 === 0) {
-    const li = document.createElement('li');
+    const li = document.createElement("li");
     li.append("Buzz");
     ul.append(li);
-    li.style.color = "blue";
+    li.style.background = "#FFD166";
     console.log("Buzz");
   } else if (i % 3 === 0 && i % 5 === 0) {
-    const li = document.createElement('li');
+    const li = document.createElement("li");
     li.append("BuzzFizz");
     ul.append(li);
-    li.style.color = "green";
-    console.log("BuzzFizz");   
+    li.style.background = "#F0466F";
+    console.log("BuzzFizz");
   } else if (i % 3 !== 0 && i % 5 !== 0) {
-    const li = document.createElement('li');
+    const li = document.createElement("li");
     li.append(i);
     ul.append(li);
-    li.style.color = "black";
+    li.style.background = "#1389B2";
     console.log(i);
   }
 }
-
-
-
